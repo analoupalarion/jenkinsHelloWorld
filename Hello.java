77f61b20a86d8677ece711ef7317f09bb6ce09fd
analoupalarion <90608744+analoupalarion@users.noreply.github.com>
2021-11-12 09:34:33 +0800

    Update Hello.java
    Made some changes

--- Hello.java
+++ Hello.java
@@ -1,14 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Tim");
-
-        int myFirstNumber = (10 + 5) + (2 *10);
-        int mySecondNumber = 12;
-        int myThirdNumber = myFirstNumber * 2;
-        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
-        System.out.println(myTotal);
-        int myLastOne = 1000 - myTotal;
-        System.out.println(myLastOne);
+        System.out.println("Hello, Analou");
+        System.out.println("You are currently learning Jenkins.");
     }
 }
